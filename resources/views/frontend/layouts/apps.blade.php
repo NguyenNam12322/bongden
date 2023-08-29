23dec47d58e50a66cfce011922a5d8ae4ffe560e
cuong <cuongdao2692@gmail.com>
2023-08-29 21:22:17 +0700

    Update apps.blade.php

--- resources/views/frontend/layouts/apps.blade.php
+++ resources/views/frontend/layouts/apps.blade.php
@@ -620,7 +620,7 @@
                                         <div class="searchform-wrapper ux-search-box relative is-normal">
                                             <form role="search" method="get" class="searchform" action="{{ route('search-product-frontend') }}">
                                                 <div class="flex-row relative">
-                                                    <div class="flex-col flex-grow"> <label class="screen-reader-text" for="woocommerce-product-search-field-0">Tìm kiếm:</label> <input type="search" id="kws" class="search-field mb-0" placeholder="Tìm kiếm&hellip;" value="" name="s" /> 
+                                                    <div class="flex-col flex-grow"> <label class="screen-reader-text" for="woocommerce-product-search-field-0">Tìm kiếm:</label> <input type="search" id="kws" class="search-field mb-0" placeholder="Tìm kiếm&hellip;" value="" name="key" /> 
                                                         <input type="hidden" name="post_type" value="product" /> </div>
                                                     <div class="flex-col"> <button type="submit" value="Tìm kiếm" class="ux-search-submit  button icon mb-0"> <i class="icon-search"></i> </button> </div>
                                                 </div>
