f4962a73d49da1a1b9d095f0077dd070f5b4274e
cuong <cuongdao2692@gmail.com>
2023-08-30 06:32:58 +0700

    update

--- resources/views/frontend/details.blade.php
+++ resources/views/frontend/details.blade.php
@@ -870,7 +870,7 @@
     <div class="clear"></div>
 </div>
 @push('script')
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+       
 
         <script type="text/javascript">
             function addToCart(id) {
