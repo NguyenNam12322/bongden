c461063c3bc23ea29610394aa3fcabb14343eb28
Cuongdao2222 <cuongdao12121@gmail.com>
2023-09-19 11:11:17 +0700

    Update details.blade.php

--- resources/views/frontend/details.blade.php
+++ resources/views/frontend/details.blade.php
@@ -72,15 +72,17 @@
             height: auto !important;
         }
 
-        .wrap-magiczoom{
-            position: static;
-            
-        }
+
     }
 
      @media only screen and (max-width: 600px) {
         .desktop{
             display: none;
+        }
+
+         .wrap-magiczoom{
+            position: static;
+            
         }
     }
 
